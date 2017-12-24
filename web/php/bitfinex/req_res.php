abf853faa4413d416e5fc7432036b9a278d0cca5
qinzian <zian.joher@gmail.com>
2017-12-24 00:13:09 -0500

    reorganized php files, so they go under one folder for a particular trading platform (diff platform folders will have their own set of php files)

---
+++ web/php/bitfinex/req_res.php
@@ -0,0 +1,32 @@
+<?php
+
+//require_once('finex.php'); // require_once <=> import once
+//require_once('config.php'); // setup vars $api_key and $api_secret
+
+
+
+
+
+/*
+$diff = $avg - $ask;
+$diff2 = $bid -  $avg;
+
+if($diff > 2){
+//good time to buy
+
+//$trade = new bitfinex($api_key, $api_secret);
+//$buy = $trade->new_order("BTCUSD", "0.01", "1", "buy", "exchange market");
+//print_r($buy);
+}
+
+if($diff2 > 2){
+//good time to sell
+echo "24h Avg: ".$avg."<br>";
+echo "Current Bid: ".$bid."<br>";
+//$trade = new bitfinex($api_key, $api_secret);
+//$sell = $trade->new_order("BTCUSD", "0.01", "1", "sell", "exchange market");
+//print_r($sell);
+}
+// */
+
+?>
