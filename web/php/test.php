316d23c9e8894ef855044465c5517b04a41b17e7
qinzian <zian.joher@gmail.com>
2017-12-26 16:05:35 -0500

    relocated files and added resources for possible future uese

---
+++ web/php/test.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>for testing purposes</title>
+  </head>
+
+	
+
+
+	<a class = "json_response">
+		<?php
+		echo "dirname start here<br>";
+		echo getcwd();
+		?>
+	</a>
+
+    <!--        Requests to be tested    >
+    account_info()
+
+
+    deposit($method, $wallet, $renew)
+    transfer()
+    margin_infos()
+    fetch_balance()
+
+    positions()
+    claim_position($position_id, $amount)
+    close_position($position_id)
+
+    new_order($symbol, $amount, $price, $side, $type)
+    cancel_order($order_id)
+    cancel_all() <!---->
+
+  </body>
+
+</html>
