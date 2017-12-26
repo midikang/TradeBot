f585884af7cb819d0e6a03a71790d592814116bf
qinzian <zian.joher@gmail.com>
2017-12-26 13:33:45 -0500

    init finance folder

---
+++ web/php/binance/get_req.php
@@ -0,0 +1,54 @@
+<?php // a set of functions that are specific to bitfinex's API
+function sendGETreq($cmd){
+  switch($cmd){
+    case "getCoinInfo":
+      return getCoinInfo($_GET["coin"], $_GET["currency"]);
+
+    case "getTradeHistory":
+      return getTradeHistory();
+
+    case "getTradingPairs":
+      return getTradingPairs();
+
+    default:
+      throw new Exception("handling unknown cmd: '".$cmd."' in ".$_GET['platform'].'/'.$_GET['reqType']."_req.php");
+
+  }
+}
+
+function getAllCoinsInfo(){
+  $url = "https://poloniex.com/public?command=returnTicker";
+
+  return getJSONstr($url);
+}
+
+function getTradingPairs() {
+  return array_keys(json_decode( getAllCoinsInfo(), true ));
+}
+
+function getCoinInfo($coin){
+  $allTicker = json_decode(getAllCoinsInfo(), true);
+
+  return json_encode($allTicker[strtoupper('btc_'.$coin)]);
+}
+
+/* add new more functions here
+function ______($coin, $currency){
+  $url = "";
+
+  return getJSONstr($url);
+}
+// */
+
+function getJSONstr($url){
+  $opts = array('http' =>
+    array(
+      'method'  => 'GET',
+      'timeout' => 10
+    )
+  );
+  $context = stream_context_create($opts);
+  $feed = file_get_contents($url, false, $context);
+  return $feed;
+}
+?>
---
+++ web/php/binance/post_req.php
@@ -0,0 +1,18 @@
+<?php // a set of functions that are specific to bitfinex's API
+require_once('zqis.php');  //setup vars $api_key and $api_secret
+require_once('poloniex.php');
+$postSender = new poloniex($api_key, $api_secret);
+
+
+function sendPOSTreq($cmd, $args){
+  switch($cmd){
+    case "getAccInfo":
+    return postSender.account_info();
+  default:
+      throw new Exception("handling unknown cmd: '".$cmd."' in ".$_GET['platform'].'/'.$_GET['reqType']."_req.php");
+  }
+
+}
+
+
+?>
