18d073d135a73dbc18aed0aeda03db5006044647
qinzian <zian.joher@gmail.com>
2017-12-26 16:11:11 -0500

    v1.2

--- python/monitor.py
+++ python/monitor.py
@@ -3,8 +3,7 @@ import time
 import datetime
 
 platform = "poloniex"
-coin = "xpm"
-currency = "btc"
+pair = 'btc_xpm'
 #finexBot = Trader("bitfinex")
 
 bot = Trader("poloniex")
@@ -14,13 +13,13 @@ startTime = round(time.time())
 
 
 timeStr = datetime.datetime.fromtimestamp(startTime).strftime('%Y-%m-%d %H:%M:%S')
-print('''\t{}: {}-{}\t{}\n{}\n'''.format(platform, coin, currency, timeStr,bot.getCoinInfo(coin, currency)))
+print('''\t{}: {}\t{}\n{}\n'''.format(platform, pair, timeStr,bot.getCoinInfo(pair)))
 
 while(1):
 	crTime = round(time.time())
 	diff = crTime - startTime
 	if (diff > 0 and diff%rate == 0): # every rate seconds
 		timeStr = datetime.datetime.fromtimestamp(crTime).strftime('%Y-%m-%d %H:%M:%S')
-		print('''\t{}: {}-{}\t{}\n{}\n'''.format(platform, coin, currency, timeStr,bot.getCoinInfo(coin, currency)))
+		print('''\t{}: {}\t{}\n{}\n'''.format(platform, pair, timeStr,bot.getCoinInfo(pair)))
 
 		startTime = crTime
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 project5: crypto-trading bot
 
-Next v1.1 (Backend):
-- timed call of GET request and display parsed info
+Next v1.2 (Backend):
+- write a sample process that decides how to make money
 
 - try to send POST requests for other things (e.g. acc info)
 
@@ -9,8 +9,6 @@ Next v1.1 (Backend):
 Basic Design:
 	(Main - Python)
 - Where the main processing is handled
-
-- Can send GET request on its own
 
 - Using localhost to send requests to trading platforms via different .php files
 
@@ -21,7 +19,7 @@ Basic Design:
 
 
 Issues:
-- sending POST requests using python to bitfinex's server
+- sending POST requests using python to specified servers
 
 Credit:
 	- github |coinables|: php wrapper for bitfinex API
