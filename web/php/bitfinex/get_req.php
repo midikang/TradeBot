20e6285be00cd400588beaae2d58ff93f7313555
qinzian <zian.joher@gmail.com>
2018-01-02 00:18:58 -0500

    renamed getCoinInfo() to getOrderBook()

--- web/php/bitfinex/get_req.php
+++ web/php/bitfinex/get_req.php
@@ -1,8 +1,8 @@
 <?php // a set of functions that are specific to bitfinex's API
 function call_func($cmd){
   switch($cmd){
-    case "getCoinInfo":
-      return getCoinInfo($_GET["coin"].$_GET["currency"]);
+    case "getOrderBook":
+      return getOrderBook($_GET["coin"].$_GET["currency"]);
 
     case "getValidPairs":
     return getValidPairs();
@@ -12,7 +12,7 @@ function call_func($cmd){
   }
 }
 
-function getCoinInfo($pair){
+function getOrderBook($pair){
   $url = "https://api.bitfinex.com/v1/book/".$pair;
 
   return getJSONstr($url);
