e276c2a6041df7001493c691e24c85f7b6a5f9fb
qinzian <zian.joher@gmail.com>
2018-01-02 13:54:06 -0500

    getJSONstr will now be able to send POST requests, this may replace use of curl in bitfinex and poloniex's php wrapper

--- web/php/gen_func.php
+++ web/php/gen_func.php
@@ -1,13 +1,14 @@
 <?php // functions that gets used by all php scripts
 
-function getJSONstr($url, $headers = ""){
+function getJSONstr($url, $headers = "", $method = "GET"){
   $opts = array('http' =>
     array(
-      'method'  => 'GET',
+      'method'  => $method,
       'timeout' => 10,
       'header'  => $headers
     )
   );
+
   $context = stream_context_create($opts);
   $jsonStr = file_get_contents($url, false, $context);
   return $jsonStr;
