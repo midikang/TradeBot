f962d2866e4dd14aeaf27a609b4252d46cfe1661
qinzian <zian.joher@gmail.com>
2018-01-11 23:29:24 -0500

    made sure orderbook pair arg is lower case

--- web/php/yobit/private_req.php
+++ web/php/yobit/private_req.php
@@ -10,10 +10,10 @@ function call_func($cmd){
       return $GLOBALS['postSender']->getBalances();
 
     case "buy":
-      return $GLOBALS['postSender']->buy($_GET['coin']."_".$_GET['currency'], $_GET['amt'], $_GET['price']);
+      return $GLOBALS['postSender']->buy(strtolower($_GET['coin']."_".$_GET['currency']), $_GET['amt'], $_GET['price']);
 
     case "sell":
-      return $GLOBALS['postSender']->sell($_GET['coin']."_".$_GET['currency'], $_GET['amt'], $_GET['price']);
+      return $GLOBALS['postSender']->sell(strtolower($_GET['coin']."_".$_GET['currency']), $_GET['amt'], $_GET['price']);
 
     case "withdraw":
       return $GLOBALS['postSender']->withdraw();
