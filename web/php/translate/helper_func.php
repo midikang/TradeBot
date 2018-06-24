7865a92e7f917c094565521215f4d4f7ee0882e1
qinzian <zian.joher@gmail.com>
2018-06-24 18:52:09 -0400

    added function that imports csv into table

---
+++ web/php/translate/helper_func.php
@@ -0,0 +1,58 @@
+<?php
+require_once 'connection.php';
+
+function importCSVintoTable($infile,$table_name)
+{
+  echo "import $infile into translate.$table_name<br>";
+  $cxn = openDBCxn("translate");
+
+  $sql = "load data infile '$infile' into table $table_name
+  fields terminated by ','
+  lines terminated by '\n'";
+
+  if ($cxn->query($sql)){
+    $cxn->close();
+  } else {
+    die("importing failed<br>$cxn->error");
+  }
+}
+
+function getPlatforms()
+{
+  echo "getting all platforms<br>";
+
+  $cxn = openDBCxn("translate");
+
+  $sql = "select platform from platforms";
+
+  $result = $cxn->query($sql);
+
+  $index = 0;
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $platforms[$index] = $row['platform'];
+
+        $index += 1;
+    }
+
+    $cxn->close();
+    return $platforms;
+
+  } else {
+    echo "0 rows of results";
+  }
+  $cxn->close();
+}
+
+function freeMultiQueryNoResult($cxn)
+{
+  do{
+      if ($result = mysqli_store_result($cxn)){
+        mysqli_free_result($result);
+      }
+  } while (mysqli_next_result($cxn));
+}
+#print_r(getPlatforms());
+
+?>
