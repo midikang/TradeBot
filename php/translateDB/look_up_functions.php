072e7e237ed1465bbd23b101133a359b1b15a34a
qinzian <zian.joher@gmail.com>
2018-06-24 23:30:46 -0400

    finished implementing all funcs and tested them

--- php/translateDB/look_up_functions.php
+++ php/translateDB/look_up_functions.php
@@ -5,20 +5,27 @@ function executeSelect($select_field, $select_table, $cond_field, $cond_value){
   $cxn = OpenDBCxn();
 
   $sql = "select $select_field from $select_table where $cond_field = '$cond_value'";
-  #$sql = "select int_repr from int2name where coin_name = 'bitcoin'";
+  #$sql = "select coin_name from int2name where coin_name = 'bitcoin'";
 
-  #echo $sql;
+  echo "<br>$sql<br>";
 
   $result = $cxn->query($sql);
 
-  if ($result->num_rows > 0) {
+  if ($result and $result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         return $row[$select_field];
     }
-  } else {
-    echo "0 rows of results";
-    return;
+  } else { // return default values
+    if ($select_field == "int_repr"){
+      return 0;
+    }
+    if ($select_field == "coin_name"){
+      return "name";
+    }
+    if ($select_field == "coin_alias"){
+      return "alias";
+    }
   }
 
   $cxn->close();
@@ -28,28 +35,25 @@ function getIntWithName($name){
   return executeSelect("int_repr", "int2name", "coin_name", $name);
 }
 
-echo $GLOBALS["dbname"]."<br>";
-echo "lala:".getIntWithName("bitcoin");
+function getNameWithInt($int){
+  return executeSelect("coin_name", "int2name", "int_repr", $int);
+}
+
+function getNameWithAlias($platform, $alias){
+  return executeSelect("coin_name", $platform, "coin_alias", $alias);
+}
+
+function getAliasWithName($platform, $name){
+  return executeSelect("coin_alias", $platform, "coin_name", $name);
+}
 
 function getIntWithAlias($platform, $alias){
-  return executeSelect("int_repr", $platform, "coin_alias", $alias);
+  return getIntWithName(getNameWithAlias($platform,$alias));
 }
 
-function getNameWithInt(){
-
+function getAliasWithInt($platform, $int){
+  return getAliasWithName($platform,getNameWithInt($int));
 }
 
-function getNameWithAlias(){
-
-}
-
-function getAliasWithName(){
-
-}
-
-function getAliasWithInt(){
-
-}
-
-
+// echo getIntWithAlias("bitfinex","btc"); // 33
 ?>
