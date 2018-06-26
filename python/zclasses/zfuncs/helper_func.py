f4c3f95a0751787a29285ef90aaee91220d00b39
qinzian <zian.joher@gmail.com>
2018-06-25 23:25:02 -0400

    moved the function sendReq() from trader to here

--- python/zclasses/zfuncs/helper_func.py
+++ python/zclasses/zfuncs/helper_func.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 from sys import stderr
+
+import requests
 import json
+from lxml import html
 
 def pathToString(pathTP):
     lst = list(map(str,pathTP))
@@ -21,3 +24,16 @@ def reFormatJSON(pyRes, keys = []):
                 tmp[key] = pyRes[key];
 
     return json.dumps(tmp, sort_keys=True, indent=4)
+
+def sendReq(self, url):
+    #print('\n'+url)
+    response = requests.get(url)
+
+    # .content gives the response in bytes
+    tree = html.fromstring(response.content)
+    #print(response.text)
+
+    jsonResStr = tree.xpath('//a[@class="json_response"]/text()')[0] # there's only 1 item in the returned list from xpath
+    #print(jsonResStr)
+
+    return json.loads(jsonResStr) # response as python obj
