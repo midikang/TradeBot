74a01e274416f9072c255246500e51b8a234e752
qinzian <zian.joher@gmail.com>
2018-06-26 20:21:15 -0400

    decided to move the php switch() code to another file under translateDB/

--- php/translateDB.php
+++ php/translateDB.php
@@ -8,13 +8,11 @@
   <body>
     <a class = "json_response">
       <?php
-        switch($_GET['cmd']){
-          case "setup_db_translate":
-            require_once("translateDB/setup_db_translate.php");
-
-          case "getPlatforms":
-            require_once("translateDB/helper_func.php");
-            echo json_encode(getPlatforms());
+        if($_GET['cmd'] = setup_db_translate){
+          require_once("translateDB/setup_db_translate.php");
+        } else {
+          require_once(translateDB/db_funcs.php);
+          echo json_encode(call_func($_GET['cmd']));
         }
       ?>
     </a>
