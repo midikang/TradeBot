2a9b4349a4433e9e7645c73b0d1b68e8c7a7d3ff
qinzian <zian.joher@gmail.com>
2018-06-28 11:10:45 -0400

    similar to the translateDB.py except it's for the pathDB

---
+++ python/zclasses/zfuncs/pathDB.py
@@ -0,0 +1,35 @@
+from zclasses.zfuncs.helper_func import sendReq
+
+def getPathDBUrl(cmdAndArgs):
+    return "{}cmd={}".format("http://localhost/tradebot/php/pathDB.php?",cmdAndArgs)
+
+def getPathUrl(cmdAndArgs):
+    return "{}cmd={}".format("http://localhost/tradebot/php/path.php?", cmdAndArgs)
+
+def isValidUser(uid,pw):
+    return sendReq(getPathDBUrl('isValidUser&uid={}&pw={}'.format(uid,pw)))
+
+def selectPath(uid):
+    return sendReq(getPathDBUrl('selectPath&uid={}'.format(uid)))
+
+def insertPath(path_jsonStr): #
+    sendReq(getPathUrl('insertPath&path_jsonStr={}'.format(path_jsonStr))
+
+def deletePath(pid): #
+    sendReq(getPathUrl('deletePath&pid={}'.format(pid))
+
+
+
+"""     The following functions may not be used here, but it will be here regardless
+def insertMonitor(uid,pid):
+    sendReq(getPathUrl('insertMonitor&uid={}&pid={}'.format(uid,pid))
+
+def insertUser(uid,pw): # may not be used
+    sendReq(getPathUrl('insertMonitor&uid={}&pid={}'.format(uid,pid))
+
+def deleteMonitor(uid,pid):
+    sendReq(getPathUrl('deleteMonitor&uid={}&pid={}'.format(uid,pid))
+
+def deleteUser(uid,pw): # may not be used
+    sendReq(getPathUrl('deleteMonitor&uid={}&pid={}'.format(uid,pid))
+"""
