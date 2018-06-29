d627f998e795d2644a824df356d203656ef736bc
qinzian <zian.joher@gmail.com>
2018-06-29 00:08:29 -0400

    made it only import json.dumps and json.loads rather than all of json

--- python/zclasses/zfuncs/helper_func.py
+++ python/zclasses/zfuncs/helper_func.py
@@ -2,17 +2,17 @@ from __future__ import print_function
 from sys import stderr
 
 import requests
-import json
+from json import dumps,loads
 from lxml import html
 
 def eprint(*args, **kwargs):
     print(*args, file=stderr, **kwargs)
 
 def json_encode(json_obj):
-    return json.dumps(json_obj)
+    return dumps(json_obj)
 
 def json_decode(json_str):
-    return json.loads(json_str)
+    return loads(json_str)
 
 def sendReceiveReq(url):
     response = sendReq(url)
@@ -24,7 +24,7 @@ def sendReceiveReq(url):
     jsonResStr = tree.xpath('//a[@class="json_response"]/text()')[0] # there's only 1 item in the returned list from xpath
     #print(jsonResStr)
 
-    return json.loads(jsonResStr) # response as python obj
+    return loads(jsonResStr) # response as python obj
 
 def sendReq(url):
     #print(url+"\n")
