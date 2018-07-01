7b7b4c596dded543184fdf36dd16e8a67361505c
qinzian <zian.joher@gmail.com>
2018-06-30 23:25:43 -0400

    commented out some functions because they don't seem like they will be used

--- python/zclasses/zfuncs/pathDB.py
+++ python/zclasses/zfuncs/pathDB.py
@@ -6,21 +6,20 @@ def getPathDBUrl(cmdAndArgs):
 def getPathUrl(cmdAndArgs):
     return "{}cmd={}".format("http://localhost/tradebot/php/path.php?", cmdAndArgs)
 
-def isValidUser(uid,pw):
-    return sendReceiveReq(getPathDBUrl('isValidUser&uid={}&pw={}'.format(uid,pw)))
-
 def selectPath(uid):
     return sendReceiveReq(getPathDBUrl('selectPath&uid={}'.format(uid)))
 
 def insertPath(path_jsonStr): #
     sendReq(getPathUrl('insertPath&path_jsonStr={}'.format(path_jsonStr))
 
-def deletePath(pid): #
-    sendReq(getPathUrl('deletePath&pid={}'.format(pid))
 
 
 
-"""     The following functions may not be used here, but it will be here regardless
+
+"""     The following functions may not be used here, but it will be implemented regardless
+def isValidUser(uid,pw):
+    return sendReceiveReq(getPathDBUrl('isValidUser&uid={}&pw={}'.format(uid,pw)))
+    
 def insertMonitor(uid,pid):
     sendReq(getPathUrl('insertMonitor&uid={}&pid={}'.format(uid,pid))
 
@@ -29,6 +28,9 @@ def insertUser(uid,pw): # may not be used
 
 def deleteMonitor(uid,pid):
     sendReq(getPathUrl('deleteMonitor&uid={}&pid={}'.format(uid,pid))
+
+def deletePath(pid): #
+    sendReq(getPathUrl('deletePath&pid={}'.format(pid))
 
 def deleteUser(uid,pw): # may not be used
     sendReq(getPathUrl('deleteMonitor&uid={}&pid={}'.format(uid,pid))
