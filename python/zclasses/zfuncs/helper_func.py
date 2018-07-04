9df3541ef889111d14a6ed07a2aaaa8542bf604b
qinzian <zian.joher@gmail.com>
2018-07-04 13:47:05 -0400

    added comments and removed useless imports now that responses are no longer html

--- python/zclasses/zfuncs/helper_func.py
+++ python/zclasses/zfuncs/helper_func.py
@@ -3,21 +3,24 @@ from sys import stderr
 
 import requests
 from json import dumps,loads
-from lxml import html
 
 def eprint(*args, **kwargs):
     print(*args, file=stderr, **kwargs)
 
-def json_encode(json_obj):
+def json_encode(json_obj): # rename
     return dumps(json_obj)
 
-def json_decode(json_str):
+def json_decode(json_str): # rename
     return loads(json_str)
 
 def sendReceiveReq(url):
-    response = sendReq(url)
+    """ sends and loads a json response from the given url,
+        url corresponds to a local php script stored on the server,
+        all such php scripts will only return objects that have been json_encoded """
+        
+    json_response = sendReq(url)
 
-    return response.json() # response as python obj
+    return json_response.json() # json str response as python obj
 
 def sendReq(url):
     #print(url+"\n")
