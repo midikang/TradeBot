e2bab7fe09298304a53e682e971c56cd08d5ab49
qinzian <zian.joher@gmail.com>
2018-07-04 16:06:36 -0400

    used another quote symbol instead of multiple single quotes for a large string

--- UI/templates/z-filters.js
+++ UI/templates/z-filters.js
@@ -1,12 +1,11 @@
-var z_filtersHTML =''+
-'<div class = "filterContainer zone">'+
-  '<a class = "title"> Filters: </a>'+
-  'Platform 1: <input class = "filter" type="text" ng-model = "view.platform1" >'+
-  '<button id = "swapPlatButton" ng-click = swapPlatforms()>Swap</button>' +
-  'Platform 2: <input class = "filter" type="text" ng-model = "view.platform2" >'+
-  'Alias 1: <input class = "filter" type="text" ng-model = "view.alias1" >'+
-  'Alias 2: <input class = "filter" type="text" ng-model = "view.alias2" >'+
-  'Alias 3: <input class = "filter" type="text" ng-model = "view.alias3" >'+
-  'Alias 4: <input class = "filter" type="text" ng-model = "view.alias4" >'+
-'</div>'
-;
+var z_filtersHTML = `
+<div class = "filterContainer zone">
+  <a class = "title"> Filters: </a>
+  Platform 1: <input class = "filter" type="text" ng-model = "view.platform1" >
+  <button id = "swapPlatButton" ng-click = swapPlatforms()>Swap</button>
+  Platform 2: <input class = "filter" type="text" ng-model = "view.platform2" >
+  Alias 1: <input class = "filter" type="text" ng-model = "view.alias1" >
+  Alias 2: <input class = "filter" type="text" ng-model = "view.alias2" >
+  Alias 3: <input class = "filter" type="text" ng-model = "view.alias3" >
+  Alias 4: <input class = "filter" type="text" ng-model = "view.alias4" >
+</div>`;
--- UI/templates/z-select.js
+++ UI/templates/z-select.js
@@ -1,5 +1,5 @@
-var z_selectHTML = ''+
-'<div class="selectContainer zone">  '+
-  '<button class = "pathSelector" ng-click="view.changeMode(\"paths\")">All Paths</button>'+
-  '<button class = "pathSelector" ng-click="view.changeMode(\"monitors\")">My Monitors</button>'+
-'</div>';
+var z_selectHTML = `
+<div class="selectContainer zone">
+  <button class = "pathSelector" ng-click="view.changeMode('paths')">All Paths</button>
+  <button class = "pathSelector" ng-click="view.changeMode('monitors')">My Monitors</button>
+</div>`;
