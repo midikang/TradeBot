634db32527c2b523ed383384b8ee386c61f8862a
qinzian <zian.joher@gmail.com>
2018-07-04 14:25:27 -0400

    redid design added new button 'swap'

--- UI/templates/z-filters.js
+++ UI/templates/z-filters.js
@@ -1,11 +1,12 @@
 var z_filtersHTML =''+
 '<div class = "filterContainer zone">'+
-  '<p class = "title"> Filters: </p>'+
-  '<ol class = "platformFilters zone" ng-repeat="i in [1,2]">'+
-    'Platform {{i}}: <input class = "filter" type="text" ng-model = "view.platform"+{{i}} >'+
-  '</ol>'+
-  '<ol class = "aliasFilters zone" ng-repeat="i in [1,2,3,4]">'+
-    'Alias {{i}}: <input class = "filter" type="text" ng-model = "view.alias"+{{i}} >'+
-  '</ol>'+
+  '<a class = "title"> Filters: </a>'+
+  'Platform 1: <input class = "filter" type="text" ng-model = "view.platform1" >'+
+  '<button id = "swapPlatButton" ng-click = swapPlatforms()>Swap</button>' +
+  'Platform 2: <input class = "filter" type="text" ng-model = "view.platform2" >'+
+  'Alias 1: <input class = "filter" type="text" ng-model = "view.alias1" >'+
+  'Alias 2: <input class = "filter" type="text" ng-model = "view.alias2" >'+
+  'Alias 3: <input class = "filter" type="text" ng-model = "view.alias3" >'+
+  'Alias 4: <input class = "filter" type="text" ng-model = "view.alias4" >'+
 '</div>'
 ;
