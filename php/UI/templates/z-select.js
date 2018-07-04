e6b28f25d37ed7e1c2f02c994fd49f6068730dcd
qinzian <zian.joher@gmail.com>
2018-07-04 13:10:46 -0400

    moved ui folder

--- php/UI/templates/z-select.js
+++
@@ -1,5 +0,0 @@
-var z_selectHTML = ''+
-'<div class="selectContainer zone">  '+
-  '<button class = "pathSelector" ng-click="view.changeMode(\"paths\")">All Paths</button>'+
-  '<button class = "pathSelector" ng-click="view.changeMode(\"monitors\")">My Monitors</button>'+
-'</div>';
