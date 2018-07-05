8482d1bdff11055345806cc5bd0f9d01d93d56b0
qinzian <zian.joher@gmail.com>
2018-07-05 12:50:04 -0400

    fixed syntax on equality comparison

--- UI/templates/z-display.js
+++ UI/templates/z-display.js
@@ -1,6 +1,6 @@
 var z_displayHTML = `
 <div class = "displayContainer zone">
-  <ul class="pathsContainer" ng-show = "view.mode = 'paths'">
+  <ul class="pathsContainer" ng-show = "view.mode == 'paths'"> Paths
       <ol class = "path" ng-repeat="() in masterList.paths">
         <button class = "addButton" ng-click=addToMonitors(path.pid)</button>
         <p class = "platform">{{path.plat1}}</a>
@@ -8,9 +8,9 @@ var z_displayHTML = `
         <p class = "platform">{{path.plat2}}</a>
       </ol>
   </ul>
-  <ul class="monitorsContainer" ng-show = "view.mode = 'monitors'">
+  <ul class="monitorsContainer" ng-show = "view.mode == 'monitors'"> Monitors
       <ol class = "monitor" ng-repeat="monitor in masterList.monitors">
-        <button class = "rmButton" ng-click=rmFromMonitors(monitor)</button>
+        <button class = "rmButton" ng-click=rmFromMonitors(monitor.pid)</button>
         <p class = "monitorRate" ng-click = "setMonitorRate(monitor)">{{monitor.rate}}</p>
         <p class = "platform">{{monitor.plat1}}</a>
         <p class = "pathStr">{{monitor.pathStr}}</a>
