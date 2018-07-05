1b808278ad05ccc851e892d228eda2e61d96b378
qinzian <zian.joher@gmail.com>
2018-07-05 11:25:32 -0400

    removed every function that has to do with crossPlat table

--- python/send_paths_to_pathDB.py
+++ python/send_paths_to_pathDB.py
@@ -1,10 +1,8 @@
 from sys import stdin
 from zclasses.zfuncs.helper_func import eprint
-from zclasses.zfuncs.pathDB import insertPath, insertCrossPlat, getMostRecentPid
+from zclasses.zfuncs.pathDB import insertPath
 
 eprint("reading paths in json_str")
-
-pid = 1 + getMostRecentPid()
 
 isFirstLine = True
 for line in stdin:
@@ -15,8 +13,4 @@ for line in stdin:
 
 
     eprint(line)
-    insertPath(line) # the auto generated pid for line is same as the var pid here
-    insertCrossPlat(plat1,plat2,pid)
-
-
-    pid+=1 # the value of 'pid' is now greater that of the most recent line
+    insertPath(plat1,plat2,line)
