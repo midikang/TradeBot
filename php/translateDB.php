9e1ef211f2d7352f482596d66f8789c4152edd89
qinzian <zian.joher@gmail.com>
2018-07-05 11:03:31 -0400

    removed option to call the db setup scripts

--- php/translateDB.php
+++ php/translateDB.php
@@ -1,8 +1,4 @@
 <?php
-  if($_GET['cmd'] == "setup_db_translate"){
-    require_once("translateDB/setup_db_translate.php");
-  } else {
-    require_once("translateDB/db_funcs.php");
-    echo json_encode(call_func($_GET['cmd']));
-  }
+require_once("translateDB/db_funcs.php");
+echo json_encode(call_func($_GET['cmd']));
 ?>
