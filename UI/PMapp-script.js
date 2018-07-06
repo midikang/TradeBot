66c801e60d81f4311826ff9043b9ad4fbabe773c
qinzian <zian.joher@gmail.com>
2018-07-06 00:01:47 -0400

    added a url for path to php script that retrieves all paths data from mysql db

--- UI/PMapp-script.js
+++ UI/PMapp-script.js
@@ -3,13 +3,19 @@ PMapp = angular.module("PathManager",[]);
 PMapp.factory("ViewSettings",function(){
   let factory = {};
 
+  // set path to local php  scripts
+  factory.selectPathsURL="http://localhost/tradebot/php/pathDB.php?cmd=selectAllPaths";
+
+
+
+
   factory.platform1 = "";
   factory.platform2 = "";
   factory.alias1 = "";
   factory.alias2 = "";
   factory.alias3 = "";
   factory.alias4 = "";
-  
+
   factory.mode = "paths";
   factory.changeMode = function(mode){
     if (["paths","monitors"].contains(mode)){
