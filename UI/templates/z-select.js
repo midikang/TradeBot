f4dea64018147d54b4cc4824499b286d3bb96f59
qinzian <zian.joher@gmail.com>
2018-07-06 11:08:44 -0400

    added <br> in between the 2 buttons and so changed the abs positioning to relative positioning

--- UI/templates/z-select.js
+++ UI/templates/z-select.js
@@ -1,4 +1,5 @@
 var z_selectHTML = `
 <button id = "ps" class = "selector" ng-click="view.changeMode('paths')">All Paths</button>
+<br>
 <button id = "ms" class = "selector" ng-click="view.changeMode('monitors')">My Monitors</button>
 `;
