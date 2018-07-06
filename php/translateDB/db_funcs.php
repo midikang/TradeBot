c9569c12481fe6468b5816e3e62d1b5bbea3389d
qinzian <zian.joher@gmail.com>
2018-07-06 12:44:46 -0400

    implemented a getInt2Name()

--- php/translateDB/db_funcs.php
+++ php/translateDB/db_funcs.php
@@ -6,6 +6,9 @@ function call_func($cmd){
     case "getPlatforms":
       return getPlatforms();
 
+    case "getInt2Name":
+      return getInt2Name();
+
     default:
         die("unrecognized translate command:       $cmd");
   }
@@ -21,8 +24,13 @@ function getPlatforms()
 
   $result = $cxn->query($sql);
 
-  $index = 0;
+  if (!$result){
+    die("sql result error in\t\tgetPlatforms()\n$cxn->error");
+  }
+
+  $platforms = array();
   if ($result->num_rows > 0) {
+    $index = 0;
     // output data of each row
     while($row = $result->fetch_assoc()) {
         $platforms[$index] = $row['platform'];
@@ -30,12 +38,31 @@ function getPlatforms()
         $index += 1;
     }
 
-    $cxn->close();
-    return $platforms;
-
-  } else {
-    echo "0 rows of results";
   }
   $cxn->close();
+  return $platforms;
+}
+
+function getInt2Name()
+{
+  $cxn = openDBCxn();
+
+  $sql = "select int_repr, coin_name from int2name";
+
+  $result = $cxn->query($sql);
+
+  if (!$result){
+    die("sql result error in\t\tgetInt2Name()\n$cxn->error");
+  }
+
+  $int2name = array();
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $int2name[$row['int_repr']] = $row['coin_name'];
+    }
+  }
+  $cxn->close();
+  return $int2name;
 }
 ?>
