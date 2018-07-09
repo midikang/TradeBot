bc6577e55a009447db8f3d805f214b76d0a66cf5
qinzian <zian.joher@gmail.com>
2018-07-08 23:50:14 -0400

    added new code to load int2alias dictionaries but there is issue due to ajax request's async

--- UI/PMapp-script.js
+++ UI/PMapp-script.js
@@ -2,17 +2,33 @@ PMapp = angular.module("PathManager",[]);
 
 PMapp.factory("ViewSettings",function(){
   let factory = {};
-  $.post("http://localhost/tradebot/php/translateDB.php?cmd=getPlatforms",{},function(res){
-    factory.platforms = res;
+
+  factory.int2alias = {};
+  factory.platforms = {};
+  factory.int2name = {};
+
+  // load platforms and int2alias dictionary for each platforms
+  $.post("http://localhost/tradebot/php/translateDB.php?cmd=getPlatforms",{},function(result){
+    factory.platforms = result;
+
+    for (let i = 0; i < factory.platforms.length; i++){
+      let plat = factory.platforms[i];
+      $.post(`http://localhost/tradebot/php/translateDB.php?cmd=getInt2Alias&platform=${plat}`,{},function(res){
+        factory.int2alias[plat] = res;
+      },"json");
+    }
   },"json");
 
+  // load int2name dictionary
   $.post("http://localhost/tradebot/php/translateDB.php?cmd=getInt2Name",{},function(res){
     factory.int2name = res;
   },"json");
 
+  console.log(factory.platforms);
+  console.log(factory.int2name);
+  console.log(factory.int2alias);
   // function to get user id and pw here
-
-  factory.sensinfo = {"rate":-1, "pid":-1, "uid":"tester", "pw":"somethingneat"};
+  factory.sensinfo = {"rate":-1, "pid":-1, "uid":"tester", "pw":"somethingneat"}; // default
 
 
   factory.platform1 = "";
