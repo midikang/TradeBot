a2113fd658c87517b13b326de2cb1fe25d802db2
qinzian <zian.joher@gmail.com>
2018-07-10 18:24:34 -0400

    reduced alias filters from 4->2

--- UI/PMapp-script.js
+++ UI/PMapp-script.js
@@ -26,8 +26,6 @@ PMapp.factory("ViewSettings",function(){
   factory.platform2 = "";
   factory.alias1 = "";
   factory.alias2 = "";
-  factory.alias3 = "";
-  factory.alias4 = "";
 
   factory.mode = "";
   factory.changeMode = function(mode){
