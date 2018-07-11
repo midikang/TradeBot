d522bd7d1eb8a23d67cfc924bfd06dd0762ecec6
qinzian <zian.joher@gmail.com>
2018-07-10 22:11:21 -0400

    swapped the <input> for plaform filter to <select>

--- UI/templates/z-filters.js
+++ UI/templates/z-filters.js
@@ -1,8 +1,7 @@
 var z_filtersHTML = `
-Platforms: <input id = "pf1" class = "filter" type="text" ng-model = "view.platform1" >
-<input id = "pf2" class = "filter" type="text" ng-model = "view.platform2" >
-<br>
-<br>
-Aliases: <input id = "af1" class = "filter" type="text" ng-model = "view.alias1" >
+<select id = "pf1" class = "filter" ng-model="view.platform1" ng-options="x for x in platforms"></select>
+<select id = "pf2" class = "filter" ng-model="view.platform2" ng-options="x for x in platforms"></select>
+- Filters -
+<input id = "af1" class = "filter" type="text" ng-model = "view.alias1" >
 <input id = "af2" class = "filter" type="text" ng-model = "view.alias2" >
 `;
