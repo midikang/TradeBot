3d8d3d28a982859cc3159f7bebf5d85e4b0557c1
qinzian <zian.joher@gmail.com>
2018-07-10 22:10:43 -0400

    moved the 2 buttons side by side horizontally

--- UI/templates/z-select.js
+++ UI/templates/z-select.js
@@ -1,5 +1,4 @@
 var z_selectHTML = `
 <button id = "ps" class = "selector" ng-click="view.changeMode('paths')">All Paths</button>
-<br>
 <button id = "ms" class = "selector" ng-click="view.changeMode('monitors')">My Monitors</button>
 `;
