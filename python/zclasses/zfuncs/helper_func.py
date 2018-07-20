102ec7cf0d980e4f60c6834747eff7cd97ad3985
qinzian <zian.joher@gmail.com>
2018-07-20 18:14:57 -0400

    added a error handler in sendReceiveReq()

--- python/zclasses/zfuncs/helper_func.py
+++ python/zclasses/zfuncs/helper_func.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from sys import stderr
 
 import requests
-from json import dumps,loads
+from json import dumps,loads,decoder
 
 def eprint(*args, **kwargs):
     print(*args, file=stderr, **kwargs)
@@ -17,10 +17,14 @@ def sendReceiveReq(url):
     """ sends and loads a json response from the given url,
         url corresponds to a local php script stored on the server,
         all such php scripts will only return objects that have been json_encoded """
-        
-    json_response = sendReq(url)
 
-    return json_response.json() # json str response as python obj
+    try:
+        json_response = sendReq(url)
+
+        return json_response.json() # json str response as python obj
+
+    except decoder.JSONDecodeError:
+        eprint("\n{}\njson_response:\n{}\n".format(url,json_response.text))
 
 def sendReq(url):
     #print(url+"\n")
