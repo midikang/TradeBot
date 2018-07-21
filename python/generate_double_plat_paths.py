79e9c2b88a8bc8857cb0fdb29dc4751c56290ba8
qinzian <zian.joher@gmail.com>
2018-07-21 00:03:09 -0400

    replaced calls of 'getIntWithAlias()' with single call of 'getAlias2Int()' to get the dict to work locally

--- python/generate_double_plat_paths.py
+++ python/generate_double_plat_paths.py
@@ -8,7 +8,7 @@ if amt_arg != 3 and amt_arg != 5:
     exit()
 
 from zclasses.Generator import Generator
-from zclasses.zfuncs.translateDB import getAliases, getIntWithAlias
+from zclasses.zfuncs.translateDB import getAliases, getAlias2Int
 from zclasses.zfuncs.helper_func import json_encode
 eprint("loading args")
 
@@ -18,8 +18,8 @@ amtTP1 = 1
 amtTP2 = 1
 
 if amt_arg == 5:
-    amtTP1 = sys.argv[3]
-    amtTP2 = sys.argv[4]
+    amtTP1 = int(sys.argv[3])
+    amtTP2 = int(sys.argv[4])
 
 if plat1 == "test":
     plat1 = "poloniex"
@@ -35,13 +35,14 @@ genBot.setupPlatsTPs(plat2)
 eprint("find overlapping coins")
 platformAliases = [getAliases(plat1),getAliases(plat2)]
 coinLists = [set(),set()] # set of int repr of coins in plat1 and plat2 respectively
+alias2intDicts = [getAlias2Int(plat1),getAlias2Int(plat2)]
 
 for i in range(2): # populate coinLists
     for a in platformAliases[i]:
-        coinLists[i].add(getIntWithAlias(plat1,a))
+        coinLists[i].add(alias2intDicts[i][a])
 
 overlappingCoins = coinLists[0].intersection(coinLists[1])
-
+#eprint(overlappingCoins)
 
 eprint("begin generating paths with max length {} coins across \nplatforms \t{} \tand \t{}\n\n".format(amtTP1+amtTP2,plat1,plat2))
 print("{} {}".format(plat1,plat2))
@@ -49,10 +50,11 @@ for int in overlappingCoins:
     pathsPart1 = genBot.genPathsWithInt(plat1,amtTP1,endInt = int)
 
     for pathPart1 in pathsPart1:
+        #eprint(pathPart1)
         if pathPart1[0].getHead() == int: # pathPart1 shouldn't form a loop by itself
             continue # avoid looking at these kind of paths
 
-
+        #eprint("plat2:{}({})\namtTP2:{}({})\nstartInt:{}({})\nendInt:{}({})\n".format(plat2,type(plat2),amtTP2,type(amtTP2),int,type(int),pathPart1[0].getHead(),type(pathPart1[0].getHead())))
         pathsPart2 = genBot.genPathsWithInt(plat2,amtTP2,startInt = int,
                                             endInt = pathPart1[0].getHead())
         for pathPart2 in pathsPart2:
